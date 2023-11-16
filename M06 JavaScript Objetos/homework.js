a98244e4d380eea99f2e33f92cec4a124a40122a
JoacoLarrivey <joaqlarrivey@gmail.com>
2023-11-16 20:00:51 -0300

    Homework m06 completa

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,6 +7,14 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+   var gato = {
+   nombre: nombre, 
+   edad: edad,
+   meow: function(){
+      return "Meow!"
+   } 
+};
+return gato;
 }
 
 function nuevoUsuario(nombre, email, password) {
@@ -14,7 +22,14 @@ function nuevoUsuario(nombre, email, password) {
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
    // Tu código:
+   var usuario = {
+      nombre: nombre,
+      email: email,
+      password: password,
+   };
+   return usuario;
 }
+   
 
 function agregarPropiedad(objeto, propiedad) {
    // Recibirás un objeto por parámetro.
@@ -22,6 +37,9 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   objeto[propiedad] = null;
+   return objeto;
+   
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,43 +47,73 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+      if (objeto.hasOwnProperty(metodo) && typeof objeto[metodo] === 'function') {
+          objeto[metodo]();
+      };
+  
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
+   if (objetoMisterioso && objetoMisterioso.numeroMisterioso !== undefined){
+      return objetoMisterioso.numeroMisterioso * 5;
+   }else{
+      console.error("El objeto no tiene la propiedad 'numeroMisterioso'.");
+   }
 }
+   
+
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+   delete objeto[propiedad];
+   return objeto;
 }
 
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   return objetoUsuario && objetoUsuario.email !== undefined && objetoUsuario.email !== null;
 }
+   
+
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
-}
+   return objeto.hasOwnProperty(propiedad);
+   
+ }
+   
+
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
-}
 
+      return objetoUsuario && objetoUsuario.hasOwnProperty("password") && objetoUsuario.password === password;
+  }
+  
+ 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
    // Reemplaza la contrseña guardada en la propiedad "password" del "objetoUsuario".
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
    // Tu código:
+   if (objetoUsuario && objetoUsuario.hasOwnProperty("password")){
+      objetoUsuario.password = nuevaPassword;
+   }else{
+      console.error("el objeto no tiene la propiedad 'password'.");
+   }
+   return objetoUsuario;
+   
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
@@ -73,6 +121,12 @@ function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
    // Tu código:
+   if (objetoUsuario && objetoUsuario.hasOwnProperty("amigos") && Array.isArray(objetoUsuario.amigos)){
+      objetoUsuario.amigos.push(nuevoAmigo);
+   }else{
+      console.error ("El objeto no tiene la propiedad 'amigos' o no es un arreglo.");
+   }
+   return objetoUsuario;
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -81,6 +135,14 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
    // Tu código:
+   if (Array.isArray(objetoMuchosUsuarios)){
+      objetoMuchosUsuarios.forEach(function(usuario){
+         usuario.esPremium = true;
+      });
+   }else{
+      console.error("el parámetro no es un arreglo");
+   }
+   return objetoMuchosUsuarios;
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -89,6 +151,20 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
+   if (objetoUsuario && objetoUsuario.hasOwnProperty("posts") && Array.isArray(objetoUsuario.posts)){
+      var sumaLikes = 0;
+      objetoUsuario.posts.forEach(function(post){
+         if(post.hasOwnProperty("likes") && typeof post.likes === "number"){
+            sumaLikes += post.likes;
+         }else{
+            console.error("El post no tiene la propiedad 'likes' o no es un número.");
+         }
+      });
+      return sumaLikes;
+   }else{
+      console.error("El objeto no tiene la propiedad 'posts' o no es un arreglo.");
+      return 0;
+   }
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -102,8 +178,16 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+   objetoProducto.calcularPrecioDescuento = function(){
+      var descuento = objetoProducto.precio * objetoProducto.porcentajeDeDescuento;
+      return objetoProducto.precio - descuento;
+   }
+   return objetoProducto;
+
+
 }
 
+  
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    crearGato,
